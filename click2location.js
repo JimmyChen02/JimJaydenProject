47ffee64b6fa9ab0f66b85d2bbb9cb24900ecc2b
Jayden Ai <jaydenai@brb-02792.apn.wlan.private.upenn.edu>
2023-07-19 10:20:23 -0400

    js

--- click2location.js
+++ click2location.js
@@ -1,7 +1,19 @@
 let svg = document.getElementsByTagName("svg")[0]
-svg.addEventListener('click', teleport, false);
-let ghost = document.createElement("circle");
-function teleport(event) {
-    ghost.setAttribute("cx", event.offsetX.toString());
-    ghost.setAttribute("cy", event.offsetY.toString());
+svg.addEventListener('click', addRandomGhost, false);
+let bodyWidth = 2 *headRadius;
+let bodyHeight = 100;
+let eyeSpacing = 25;
+let ghost = document.getElementsByTagName("svg").createElementNS("http://www.w3.org/2000/svg");
+function addRandomGhost(event) {
+    let cx = event.offsetX
+    let cy = event.offsetY
+    let color = color(Math.floor(colors.length * Math.random()));
+    let ghost = document.getElementsByTagName("svg").createElementNS("circle");
+    ghost.appendChild.createCircle(cx, cy, headRadius, color);
+    let body = document.createElementNUS("http://www.w3.org/2000/svg", "rect");
+    
+    body.setAttribute("x", (cx-60).toString());
+    body.setAttribute("y", (cy).toString());
+    body.setAttribute("width", bodyWidth);
+    body.setAttribute("height", bodyHeight);
 }
