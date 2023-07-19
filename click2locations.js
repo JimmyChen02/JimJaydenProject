0537854a503ff68d664788c1c1940b92cb5d9fca
Jimmy Chen <jimmy2@brb-11073.apn.wlan.private.upenn.edu>
2023-07-19 11:47:30 -0400

    move

--- click2locations.js
+++ click2locations.js
@@ -17,3 +17,22 @@ function addRandomGhost(event) {
     body.setAttribute("width", bodyWidth);
     body.setAttribute("height", bodyHeight);
 }
+
+// movement
+
+let pupil = document.getElementsByClassName("pupil")
+svg.addEventListener('mousemove', move, false)
+function move(event) {
+    let dx = 0;
+    let dy = 0;
+    if (tx != ox) {
+        let m = (ty - oy) / (tx - ox);
+        dx = Math.sign(tx - ox) * ds / Math.sqrt(1 + m * m);
+        dy = m * dx;
+    }
+    else {
+        dy = Math.sign(ty -oy)*ds;
+    }
+    let x = ox + dx;
+    let y = oy + dy; 
+}
